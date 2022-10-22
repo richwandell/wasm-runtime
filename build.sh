e2049f80f9310ccf9cbf6e7289ac941c0a3ef02f
Rich Wandell <richwandell@gmail.com>
2022-10-22 13:05:53 -0400

    2022-10-22

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+CI=$1
+
 for FILE in wat/*; do
   if [[ $FILE == *".wat"* ]]; then
     OUTFILE=${FILE/.wat/.wasm}
     OUTFILE=${OUTFILE/wat/wasm}
     echo "compiling $FILE into $OUTFILE"
-    docker run -it --rm -v "$PWD":/src -w /src kirillt/wabt wat2wasm "$FILE" -o "$OUTFILE"
+    if [[ "CI" == "$1" ]]; then
+      wat2wasm "$FILE" -o "$OUTFILE"
+    else
+      docker run -it --rm -v "$PWD":/src -w /src kirillt/wabt wat2wasm "$FILE" -o "$OUTFILE"
+    fi
   fi
 done
 
